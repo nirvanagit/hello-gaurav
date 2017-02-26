248938f13b0b0398b5b4134203ee82558cfc213d
Anubhav Aeron <a.aeron.87@gmail.com>
2017-02-26 11:43:54 -0800

    First commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nirvanagit/hello"
+	"github.com/nirvanagit/hello/util"
+)
+
+func main() {
+	fmt.Printf("Hello Gaurav\n")
+	hello.SayHi()
+	util.SayFoo()
+}
